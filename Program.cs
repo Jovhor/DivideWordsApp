24809bab36ac73ac42b9a16265ab02e5372de4a1
Jovhor <90206386+Jovhor@users.noreply.github.com>
2023-10-25 11:04:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideWordsApp
+{
+    class Program
+    {
+
+        static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+
+            foreach (var word in words)
+            {
+                Print(word);
+            }
+            return words;
+        }
+
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            SplitText(line);
+            Console.ReadKey();
+        }
+    }
+}
